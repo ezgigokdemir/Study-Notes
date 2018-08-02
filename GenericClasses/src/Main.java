0de7e01513af922d92eb08db12558a8a87e637da
ezgigokdemir <38529111+ezgigokdemir@users.noreply.github.com>
2018-08-02 21:35:32 +0300

    Add files via upload

---
+++ GenericClasses/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+   
+        Character[] char_dizi = {'J','A','V','A'};
+        Integer[] integer_dizi = {1,2,3,4,5,6};
+        
+        String[] string_dizi = {"Java","Python","C++","Php"};
+
+        Ogrenci[] ogrenci_dizi = {new Ogrenci("Tülay"),new Ogrenci("Uğur"),new Ogrenci("Ezgi"),new Ogrenci("Eylem")};
+      
+        /*
+        CharYazdir.yazdir(char_dizi);
+        System.out.println("*************************");
+        StringYazdir.yazdir(string_dizi);
+        System.out.println("*************************");
+        IntegerYazdir.yazdir(integer_dizi);
+        System.out.println("*************************");
+        OgrenciYazdir.yazdir(ogrenci_dizi);*/
+        
+        YazdirmaSinifi<Character> yazdir_char = new YazdirmaSinifi<Character>();
+        YazdirmaSinifi<Integer> yazdir_int = new YazdirmaSinifi<Integer>();
+        YazdirmaSinifi<String> yazdir_string = new YazdirmaSinifi<String>();
+        YazdirmaSinifi<Ogrenci> yazdir_ogrenci = new YazdirmaSinifi<Ogrenci>();
+
+        yazdir_char.yazdir(char_dizi);
+        System.out.println("*************************");
+        yazdir_int.yazdir(integer_dizi);
+        System.out.println("*************************");
+        yazdir_string.yazdir(string_dizi);
+        System.out.println("*************************");
+        yazdir_ogrenci.yazdir(ogrenci_dizi);
+        
+        
+        
+        
+}
+    
+}
